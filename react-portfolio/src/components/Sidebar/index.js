d1b45937671958065433b5c796d068a8e999c39d
Gerard Masdeu <43605040+GerardMasdeu@users.noreply.github.com>
2023-08-29 00:50:36 -0700

    Setting up Sidebar elements
    adding some logos

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,4 +1,13 @@
 import './index.scss'
-
-const Sidebar = () => <>Hello from sidebar</>
+import { Link } from "react-router-dom";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo'/>
+            <img src={LogoS} alt='slogan'/>
+        </Link>
+    </div>
+)
 export default Sidebar
