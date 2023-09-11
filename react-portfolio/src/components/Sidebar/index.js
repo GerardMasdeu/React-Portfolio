2727c2c18f6d9a32028e6dc6072f1b03293a0e9a
Gerard Masdeu <43605040+GerardMasdeu@users.noreply.github.com>
2023-09-11 10:35:04 -0700

    Icons
    Icons are showing up but theres something wrong with the nav part of the SASS code

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,6 @@
 import './index.scss';
 import { Link, NavLink } from "react-router-dom";
 import LogoS from "../../assets/images/logo-s.png";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome,faUser,faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,19 +8,23 @@ const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt='logo'/>
-           
+            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+            <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="aboutlink" to="/about">
-                <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+            
             </NavLink>
             <NavLink exact="true" activeclassname="active" to="/contact" className="contact-link">
-                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            
             </NavLink>
         </nav>
     </div>
 )
+      
+
 export default Sidebar;
