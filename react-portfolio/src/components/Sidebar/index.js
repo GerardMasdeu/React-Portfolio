ce3482cf28cd289a5a6a0321910aaab270980175
Gerard Masdeu <43605040+GerardMasdeu@users.noreply.github.com>
2023-09-11 20:14:16 +0200

    All works as intended now
    Icons fixed and ABOUT not showing up when hovering now works

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -2,26 +2,26 @@ import './index.scss';
 import { Link, NavLink } from "react-router-dom";
 import LogoS from "../../assets/images/logo-s.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome,faUser,faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faHome,faAddressCard,faMessage } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt='logo'/>
-            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
-            <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
-            <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="aboutlink" to="/about">
-            
+
+            <NavLink exact="true" activeclassname="active" to="/about" className="about-link" >
+            <FontAwesomeIcon icon={faAddressCard} color='#4d4d4e'/>
             </NavLink>
+
             <NavLink exact="true" activeclassname="active" to="/contact" className="contact-link">
-            
+            <FontAwesomeIcon icon={faMessage}  color='#4d4d4e'/>
             </NavLink>
+
         </nav>
     </div>
 )
